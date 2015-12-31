7e6a9c606c9a31d8e52508cb8b81725988fe1d0f
Igor Dubinskiy <akkifokkusu@gmail.com>
2015-12-31 02:17:10 -0800

    Ref support(!!!), with some additional improvements
    Closes #2.
    
    * -prefix and -root-type options added; closes #6
    * default to unexported types for package main; with above effectively closes #5
    * Commit Meta-Schema JSON (with a slight change; see json-schema/json-schema#216)
    * Use generated metaSchema type instead of bootstrapped one. such meta :dog2:
    
    The code started to get ugly, with lots of copying after it became clear that types needed as much processing as fields. Refactoring and test coverage should come soon.

--- generator.go
+++ generator.go
@@ -13,14 +13,19 @@ import (
 	"regexp"
 	"sort"
 	"strings"
+	"unicode"
 
 	"github.com/gedex/inflector"
 )
 
+//go:generate schematyper -root-type=metaSchema -prefix=meta metaschema.json
+
 var (
-	outToStdout = flag.Bool("c", false, "output to console; overrides \"-o\"")
-	outputFile  = flag.String("o", "", "output file name; default is <schema>_schematype.go")
-	packageName = flag.String("p", "main", "package name for generated file; default is \"main\"")
+	outToStdout     = flag.Bool("c", false, `output to console; overrides "-o"`)
+	outputFile      = flag.String("o", "", "output file name; default is <schema>_schematype.go")
+	packageName     = flag.String("package", "main", `package name for generated file; default is "main"`)
+	rootTypeName    = flag.String("root-type", "", `name of root type; default is generated from the filename`)
+	typeNamesPrefix = flag.String("prefix", "", `prefix for non-root types`)
 )
 
 type structField struct {
@@ -46,16 +51,23 @@ func (s structFields) Swap(i, j int) {
 }
 
 type goType struct {
-	Name    string
-	Fields  structFields
-	Comment string
+	Name     string
+	Type     string
+	Nullable bool
+	Fields   structFields
+	Comment  string
 }
 
 func (gt goType) print(buf *bytes.Buffer) {
 	if gt.Comment != "" {
-		buf.WriteString(fmt.Sprintln("//", gt.Comment))
+		buf.WriteString(fmt.Sprintf("// %s\n", gt.Comment))
 	}
-	buf.WriteString(fmt.Sprintln("type ", gt.Name, " struct {"))
+	buf.WriteString(fmt.Sprintf("type %s %s", gt.Name, gt.Type))
+	if gt.Type != "struct" {
+		buf.WriteString("\n")
+		return
+	}
+	buf.WriteString(" {\n")
 	sort.Stable(gt.Fields)
 	for _, sf := range gt.Fields {
 		var typeString string
@@ -69,7 +81,7 @@ func (gt goType) print(buf *bytes.Buffer) {
 			tagString += ",omitempty"
 		}
 		tagString += "\"`"
-		buf.WriteString(fmt.Sprintln("   ", sf.Name, "  ", typeString, "  ", tagString))
+		buf.WriteString(fmt.Sprintf("%s %s %s\n", sf.Name, typeString, tagString))
 	}
 	buf.WriteString("}\n")
 }
@@ -164,45 +176,206 @@ func camelCaseToWords(s string) string {
 	return regexp.MustCompile(`([\p{Ll}\p{N}])(\p{Lu})`).ReplaceAllString(s, "$1 $2")
 }
 
-func generateTypeName(origName string) string {
+func getExportedIdentifierPart(part string) string {
+	upperedPart := strings.ToUpper(part)
+	if commonInitialisms[upperedPart] {
+		return upperedPart
+	}
+	return strings.Title(strings.ToLower(part))
+}
+
+func generateIdentifier(origName string, exported bool) string {
 	spacedName := camelCaseToWords(dashedToWords(origName))
 	titledName := strings.Title(spacedName)
 	nameParts := strings.Split(titledName, " ")
 	for i, part := range nameParts {
-		upperedPart := strings.ToUpper(part)
-		if commonInitialisms[upperedPart] {
-			nameParts[i] = upperedPart
+		nameParts[i] = getExportedIdentifierPart(part)
+	}
+	if !exported {
+		nameParts[0] = strings.ToLower(nameParts[0])
+	}
+	rawName := strings.Join(nameParts, "")
+
+	// make sure we build a valid identifier
+	buf := &bytes.Buffer{}
+	for pos, char := range rawName {
+		if unicode.IsLetter(char) || char == '_' || (unicode.IsDigit(char) && pos > 0) {
+			buf.WriteRune(char)
 		}
 	}
-	return strings.Join(nameParts, "")
+
+	return buf.String()
 }
 
-var types goTypes
+func generateTypeName(origName string) string {
+	if *packageName != "main" || *typeNamesPrefix != "" {
+		return *typeNamesPrefix + generateIdentifier(origName, true)
+	}
 
-func getType(s *schema, pName, pDesc string) (typeName string) {
+	return generateIdentifier(origName, false)
+}
+
+func generateFieldName(origName string) string {
+	return generateIdentifier(origName, true)
+}
+
+func getTypeSchema(typeInterface interface{}) *metaSchema {
+	typeSchemaJSON, _ := json.Marshal(typeInterface)
+	var typeSchema metaSchema
+	json.Unmarshal(typeSchemaJSON, &typeSchema)
+	return &typeSchema
+}
+
+func getTypeSchemas(typeInterface interface{}) map[string]*metaSchema {
+	typeSchemasJSON, _ := json.Marshal(typeInterface)
+	var typeSchemas map[string]*metaSchema
+	json.Unmarshal(typeSchemasJSON, &typeSchemas)
+	return typeSchemas
+}
+
+func singularize(plural string) string {
+	singular := inflector.Singularize(plural)
+	if singular == plural {
+		singular += "Item"
+	}
+	return singular
+}
+
+func parseAdditionalProperties(ap interface{}) (hasAddl bool, addlSchema *metaSchema) {
+	switch ap := ap.(type) {
+	case bool:
+		return ap, nil
+	case map[string]interface{}:
+		return true, getTypeSchema(ap)
+	default:
+		return
+	}
+}
+
+type deferredType struct {
+	schema *metaSchema
+	name   string
+	desc   string
+}
+
+var types = make(map[string]goType)
+var deferredTypes = make(map[string]deferredType)
+
+func processType(s *metaSchema, pName, pDesc, path string) (typeName string) {
 	var gt goType
 
-	if pName != "" {
-		typeName = pName
-	} else {
-		typeName = s.Title
-	}
-	if gt.Name = generateTypeName(typeName); gt.Name == "" {
-		log.Fatalln("Can't generate type without name.")
+	// avoid 'recursive type' problem, at least for the root type
+	if path == "#" {
+		gt.Nullable = true
 	}
 
-	if pDesc != "" {
-		gt.Comment = pDesc
+	if s.Ref != "" {
+		if refType, ok := types[s.Ref]; ok {
+			return refType.Name
+		}
+		deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+		return ""
+	}
+
+	defer func() { types[path] = gt }()
+
+	var origTypeName string
+	if path == "#" {
+		origTypeName = *rootTypeName
+		gt.Name = *rootTypeName
 	} else {
-		gt.Comment = s.Description
+		origTypeName = s.Title
+		if origTypeName == "" {
+			origTypeName = pName
+		}
+
+		if gt.Name = generateTypeName(origTypeName); gt.Name == "" {
+			log.Fatalln("Can't generate type without name.")
+		}
+	}
+
+	typeName = gt.Name
+
+	gt.Comment = s.Description
+	if gt.Comment == "" {
+		gt.Comment = pDesc
 	}
 
 	required := make(map[string]bool)
 	for _, req := range s.Required {
-		required[req] = true
+		required[string(req)] = true
 	}
 
-	for propName, propSchema := range s.Properties {
+	var jsonType string
+	switch schemaType := s.Type.(type) {
+	case []interface{}:
+		if len(schemaType) == 2 && (schemaType[0] == "null" || schemaType[1] == "null") {
+			gt.Nullable = true
+
+			jsonType = schemaType[0].(string)
+			if jsonType == "null" {
+				jsonType = schemaType[1].(string)
+			}
+		}
+	case string:
+		jsonType = schemaType
+	}
+
+	props := getTypeSchemas(s.Properties)
+	hasProps := len(props) > 0
+	hasAddlProps, addlPropsSchema := parseAdditionalProperties(s.AdditionalProperties)
+
+	typeString := getTypeString(jsonType, s.Format)
+	switch typeString {
+	case "object":
+		if gt.Name == "Properties" {
+			panic(fmt.Errorf("props: %+v\naddlPropsSchema: %+v\n", props, addlPropsSchema))
+		}
+		if hasProps && !hasAddlProps {
+			gt.Type = "struct"
+		} else if hasAddlProps && !hasProps {
+			singularName := singularize(origTypeName)
+			gotType := processType(addlPropsSchema, singularName, s.Description, path+"/additionalProperties")
+			if gotType == "" {
+				deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+				return ""
+			}
+			gt.Type = "map[string]" + gotType
+		} else {
+			gt.Type = "map[string]interface{}"
+		}
+	case "array":
+		switch arrayItemType := s.Items.(type) {
+		case []interface{}:
+			if len(arrayItemType) == 1 {
+				singularName := singularize(origTypeName)
+				typeSchema := getTypeSchema(arrayItemType[0])
+				gotType := processType(typeSchema, singularName, s.Description, path+"/items/0")
+				if gotType == "" {
+					deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+					return ""
+				}
+				gt.Type = "[]" + gotType
+			} else {
+				gt.Type = "[]interface{}"
+			}
+		case interface{}:
+			singularName := singularize(origTypeName)
+			typeSchema := getTypeSchema(arrayItemType)
+			gotType := processType(typeSchema, singularName, s.Description, path+"/items")
+			if gotType == "" {
+				deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+				return ""
+			}
+			gt.Type = "[]" + gotType
+		default:
+			gt.Type = "[]interface{}"
+		}
+	default:
+		gt.Type = typeString
+	}
+
+	for propName, propSchema := range props {
 		sf := structField{
 			PropertyName: propName,
 			Required:     required[propName],
@@ -214,8 +387,18 @@ func getType(s *schema, pName, pDesc string) (typeName string) {
 		} else {
 			fieldName = propName
 		}
-		if sf.Name = generateTypeName(fieldName); sf.Name == "" {
+		if sf.Name = generateFieldName(fieldName); sf.Name == "" {
 			log.Fatalln("Can't generate field without name.")
+		}
+
+		if propSchema.Ref != "" {
+			if refType, ok := types[propSchema.Ref]; ok {
+				sf.Type, sf.Nullable = refType.Name, refType.Nullable
+				gt.Fields = append(gt.Fields, sf)
+				continue
+			}
+			deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+			return ""
 		}
 
 		switch propType := propSchema.Type.(type) {
@@ -231,16 +414,28 @@ func getType(s *schema, pName, pDesc string) (typeName string) {
 				sf.Type = getTypeString(jsonType.(string), propSchema.Format)
 			}
 		case string:
-			if propType == "" {
-				log.Fatalf("Can't create field %v withput type.", fieldName)
-			}
 			sf.Type = getTypeString(propType, propSchema.Format)
 		case nil:
-			log.Fatalf("Can't create field %v without type.", fieldName)
+			sf.Type = "interface{}"
 		}
+
+		refPath := path + "/properties/" + propName
+
+		props := getTypeSchemas(propSchema.Properties)
+		hasProps := len(props) > 0
+		hasAddlProps, addlPropsSchema := parseAdditionalProperties(propSchema.AdditionalProperties)
+
 		if sf.Type == "object" {
-			if len(propSchema.Properties) > 0 {
-				sf.Type = getType(propSchema, sf.Name, propSchema.Description)
+			if hasProps && !hasAddlProps {
+				sf.Type = "struct"
+			} else if hasAddlProps && !hasProps {
+				singularName := singularize(propName)
+				gotType := processType(addlPropsSchema, singularName, propSchema.Description, refPath+"/additionalProperties")
+				if gotType == "" {
+					deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+					return ""
+				}
+				sf.Type = "map[string]" + gotType
 			} else {
 				sf.Type = "map[string]interface{}"
 			}
@@ -248,15 +443,26 @@ func getType(s *schema, pName, pDesc string) (typeName string) {
 			switch arrayItemType := propSchema.Items.(type) {
 			case []interface{}:
 				if len(arrayItemType) == 1 {
-					singularName := inflector.Singularize(sf.Name)
-					sf.Type = "[]" + getType(getArrayTypeSchema(arrayItemType[0]), singularName, propSchema.Description)
+					singularName := singularize(propName)
+					typeSchema := getTypeSchema(arrayItemType[0])
+					gotType := processType(typeSchema, singularName, propSchema.Description, refPath+"/items/0")
+					if gotType == "" {
+						deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+						return ""
+					}
+					sf.Type = "[]" + gotType
 				} else {
 					sf.Type = "[]interface{}"
 				}
 			case interface{}:
-				singularName := inflector.Singularize(sf.Name)
-				schema := getArrayTypeSchema(arrayItemType)
-				sf.Type = "[]" + getType(schema, singularName, propSchema.Description)
+				singularName := singularize(propName)
+				typeSchema := getTypeSchema(arrayItemType)
+				gotType := processType(typeSchema, singularName, propSchema.Description, refPath+"/items")
+				if gotType == "" {
+					deferredTypes[path] = deferredType{schema: s, name: pName, desc: pDesc}
+					return ""
+				}
+				sf.Type = "[]" + gotType
 			default:
 				sf.Type = "[]interface{}"
 			}
@@ -265,16 +471,28 @@ func getType(s *schema, pName, pDesc string) (typeName string) {
 		gt.Fields = append(gt.Fields, sf)
 	}
 
-	types = append(types, gt)
-
 	return
 }
 
-func getArrayTypeSchema(typeInterface interface{}) *schema {
-	itemSchemaJSON, _ := json.Marshal(typeInterface)
-	var itemSchema schema
-	json.Unmarshal(itemSchemaJSON, &itemSchema)
-	return &itemSchema
+func processDeferred() {
+	for len(deferredTypes) > 0 {
+		for path, deferred := range deferredTypes {
+			name := processType(deferred.schema, deferred.name, deferred.desc, path)
+			if name != "" {
+				delete(deferredTypes, path)
+			}
+		}
+	}
+}
+
+func parseDefs(s *metaSchema) {
+	defs := getTypeSchemas(s.Definitions)
+	for defName, defSchema := range defs {
+		name := processType(defSchema, defName, defSchema.Description, "#/definitions/"+defName)
+		if name == "" {
+			deferredTypes["#/definitions/"+defName] = deferredType{schema: defSchema, name: defName, desc: defSchema.Description}
+		}
+	}
 }
 
 func main() {
@@ -289,13 +507,19 @@ func main() {
 		log.Fatalln("Error reading file:", err)
 	}
 
-	var s schema
+	var s metaSchema
 	if err := json.Unmarshal(file, &s); err != nil {
 		log.Fatalln("Error parsing JSON:", err)
 	}
 
+	parseDefs(&s)
+
 	schemaName := strings.Split(filepath.Base(flag.Arg(0)), ".")[0]
-	getType(&s, schemaName, "")
+	if *rootTypeName == "" {
+		*rootTypeName = schemaName
+	}
+	processType(&s, *rootTypeName, s.Description, "#")
+	processDeferred()
 
 	var resultSrc bytes.Buffer
 	resultSrc.WriteString(fmt.Sprintln("package", *packageName))
@@ -304,13 +528,18 @@ func main() {
 	if needTimeImport {
 		resultSrc.WriteString("import \"time\"\n")
 	}
-	sort.Stable(types)
+	typesSlice := make(goTypes, 0, len(types))
 	for _, gt := range types {
+		typesSlice = append(typesSlice, gt)
+	}
+	sort.Stable(typesSlice)
+	for _, gt := range typesSlice {
 		gt.print(&resultSrc)
 		resultSrc.WriteString("\n")
 	}
 	formattedSrc, err := format.Source(resultSrc.Bytes())
 	if err != nil {
+		fmt.Println(resultSrc.String())
 		log.Fatalln("Error running gofmt:", err)
 	}
 
@@ -319,7 +548,7 @@ func main() {
 	} else {
 		outputFileName := *outputFile
 		if outputFileName == "" {
-			compactSchemaName := strings.ToLower(generateTypeName(schemaName))
+			compactSchemaName := strings.ToLower(*rootTypeName)
 			outputFileName = fmt.Sprintf("%s_schematype.go", compactSchemaName)
 		}
 		err = ioutil.WriteFile(outputFileName, formattedSrc, 0644)
